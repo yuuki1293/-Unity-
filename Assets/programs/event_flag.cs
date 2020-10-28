00ad5ea03dd509a6a98e3722c1998f99389868c2
yuuki1293 <yuuki1293@outlook.jp>
2020-10-28 20:21:25 +0900

    update event_flag

--- Assets/programs/event_flag.cs
+++ Assets/programs/event_flag.cs
@@ -3,5 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Bluetooth_value{
-    
+    public class Bv : MonoBehaviour{
+        public static bool[] push = new bool[] {false,false,false,false,false};
+    }
 }
--- Assets/programs/read_file_test.cs
+++ Assets/programs/read_file_test.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using Bluetooth_value;
 
+//
 public class read_file_test : MonoBehaviour
 {
+    static string serial = "4523";
     string path = "C:\\Users\\yuuki\\動画再生\\Assets\\bluetooth_unity.pfu";
     float dt=0;
-    float gt=0.5;
-
-    // static async void delay_500min()
-    // {
-    //     await Task.Delay(5000);
-    // }
+    float gt=0.5f;
+    string before_deta;
 
     // Start is called before the first frame update
     void Start(){}
@@ -22,8 +21,14 @@ public class read_file_test : MonoBehaviour
     {
         dt += Time.deltaTime;
         if (dt>gt){
-            string data = File.ReadAllText(path);
-            Debug.Log("Data is " + data);
+            string deta = File.ReadAllText(path);
+            Debug.Log("Data is " + deta);
+            if(before_deta!=deta){
+                if(deta == serial + "00"){
+                    Bv.push[0] = 1;
+                }
+            }
+            before_deta = deta;
             dt=0;
         }
     }
