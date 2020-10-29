6a6d06e73ada2edfb9f3dc398d8b3e2f661199ad
yuuki1293 <yuuki1293@outlook.jp>
2020-10-29 17:40:29 +0900

    successfully open teraterm

--- Assets/programs/start_teraterm.cs
+++ Assets/programs/start_teraterm.cs
@@ -1,44 +1,27 @@
-﻿// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using static System.Diagnostics.Process;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
 
-// public class start_teraterm : MonoBehaviour
-// {
-//     Process exProcess;
-//     // Start is called before the first frame update
-//     void Start()
-//     {
+public class start_teraterm : MonoBehaviour
+{
+    Process exProcess;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ProcessStartInfo psInfo = new ProcessStartInfo() {
+            FileName = "C:\\Program Files (x86)\\teraterm\\ttermpro.exe",    // 実行するファイル 
+            Arguments = "C:\\Users\\yuuki\\動画再生\\Assets\\tera_macro_unity.ttl",    // コマンドパラメータ（引数）
+            CreateNoWindow = true,    // コンソール・ウィンドウを開かない
+            UseShellExecute = false,  // シェル機能を使用しない
+        };
+        Process p = Process.Start(psInfo);
+        p.WaitForExit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         
-//     }
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         if (exProcess == null)
-//         {
-//             string path
-//                 = Application.dataPath + "/test.wmv";
- 
-//             exProcess = new Process();
- 
-//             exProcess.StartInfo.FileName
-// 　　　　　　　 = "C:\\Users\\yuuki\\動画再生\\Assets\\tera_macro_unity.ttl";
-//             exProcess.StartInfo.Arguments = path;
- 
-//             //外部プロセスの終了を検知してイベントを発生させます.
-//             exProcess.EnableRaisingEvents = true;
-//             exProcess.Exited += exProcess_Exited;
- 
-//             //外部のプロセスを実行する
-//             exProcess.Start();
-//         }
-//     }
-
-//     void exProcess_Exited(object sender, System.EventArgs e)
-// {
-//     UnityEngine.Debug.Log("Event!");
-//     exProcess.Dispose();
-//     exProcess = null;
-// }
-// }
+    }
+}
