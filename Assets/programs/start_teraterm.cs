14074b85f152f93206ac4f98531e75b00db3f545
yuuki1293 <yuuki1293@outlook.jp>
2020-10-29 17:44:56 +0900

    無効なホストエラーを直した

--- Assets/programs/start_teraterm.cs
+++ Assets/programs/start_teraterm.cs
@@ -10,7 +10,7 @@ public class start_teraterm : MonoBehaviour
     void Start()
     {
         ProcessStartInfo psInfo = new ProcessStartInfo() {
-            FileName = "C:\\Program Files (x86)\\teraterm\\ttermpro.exe",    // 実行するファイル 
+            FileName = "C:\\Program Files (x86)\\teraterm\\ttpmacro.exe",    // 実行するファイル 
             Arguments = "C:\\Users\\yuuki\\動画再生\\Assets\\tera_macro_unity.ttl",    // コマンドパラメータ（引数）
             CreateNoWindow = true,    // コンソール・ウィンドウを開かない
             UseShellExecute = false,  // シェル機能を使用しない
