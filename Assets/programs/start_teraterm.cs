91ebd4b0e8d3db794613389885f27ddf8f8341ed
yuuki1293 <yuuki1293@outlook.jp>
2020-10-30 17:54:43 +0900

    make 2d

---
+++ Assets/programs/start_teraterm.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class start_teraterm : MonoBehaviour
+{
+    Process exProcess;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ProcessStartInfo psInfo = new ProcessStartInfo() {
+            FileName = "C:\\Program Files (x86)\\teraterm\\ttpmacro.exe",    // 実行するファイル 
+            Arguments = "C:\\Users\\yuuki\\動画再生\\Assets\\tera_macro_unity.ttl",    // コマンドパラメータ（引数）
+            CreateNoWindow = true,    // コンソール・ウィンドウを開かない
+            UseShellExecute = false,  // シェル機能を使用しない
+        };
+        Process p = Process.Start(psInfo);
+        //p.WaitForExit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# -Unity-
+Unityでの動画再生
+using Bluetooth_value;で
+Bv.push_flag[int n]が使えます。
+flag_numの値を変更することでフラグの数が変えられます。
