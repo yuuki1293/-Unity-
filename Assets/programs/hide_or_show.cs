0365ea4c5f326bc706e0aa0348827ac2336bb02e
yuuki1293 <yuuki1293@outlook.jp>
2020-11-03 14:37:18 +0900

    スプライトを消せた

---
+++ Assets/programs/hide_or_show.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bluetooth_value;
+
+public class hide_or_show : MonoBehaviour
+{
+    public int num;
+    SpriteRenderer mr;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mr = GetComponent<SpriteRenderer>();
+        if(Bv.push_flag[num])mr.material.color = new Color32(0,0,0,0);
+        else mr.material.color = new Color32(0,0,0,255);
+    }
+}
