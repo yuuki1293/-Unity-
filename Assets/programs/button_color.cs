65910eab63898f45ddac2f86faca381f1eb06b16
yuuki1293 <yuuki1293@outlook.jp>
2020-11-04 15:35:31 +0900

    add button

---
+++ Assets/programs/button_color.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bluetooth_value;
+
+public class button_color : MonoBehaviour
+{
+    public int num;
+    SpriteRenderer mr;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mr = GetComponent<SpriteRenderer>();
+        if(Bv.push_flag[num])mr.material.color = new Color32(0,100,0,255);
+        else mr.material.color = new Color32(0,0,100,255);
+    }
+}
