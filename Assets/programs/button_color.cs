aa459b523251dc80fbd833404a861bb202d9c485
yuuki1293 <yuuki1293@outlook.jp>
2020-11-06 18:54:45 +0900

    在庫管理を更新

--- Assets/programs/button_color.cs
+++ Assets/programs/button_color.cs
@@ -6,13 +6,17 @@ using Bluetooth_value;
 public class button_color : MonoBehaviour
 {
     public int num;
+    private bool urikire;
     SpriteRenderer mr;
 
     // Update is called once per frame
     void Update()
     {
         mr = GetComponent<SpriteRenderer>();
-        if(Bv.push_flag[num])mr.material.color = new Color32(0,100,0,255);
-        else mr.material.color = new Color32(0,0,100,255);
+        if(Bv.push_flag[num]||urikire)mr.material.color = new Color32(0,100,0,255);
+        else{
+            mr.material.color = new Color32(0,0,100,255);
+            urikire = true;
+        }
     }
 }
