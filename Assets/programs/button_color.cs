10c7b09051de22f8595fd0986de07700809a8224
yuuki1293 <yuuki1293@outlook.jp>
2020-11-06 19:15:50 +0900

    ボタンカラーを直した

--- Assets/programs/button_color.cs
+++ Assets/programs/button_color.cs
@@ -13,10 +13,12 @@ public class button_color : MonoBehaviour
     void Update()
     {
         mr = GetComponent<SpriteRenderer>();
-        if(Bv.push_flag[num]||urikire)mr.material.color = new Color32(0,100,0,255);
-        else{
-            mr.material.color = new Color32(0,0,100,255);
+        if (Bv.push_flag[num] || urikire)
+        {
+            mr.material.color = new Color32(0, 100, 0, 255);
             urikire = true;
         }
+        else mr.material.color = new Color32(0, 0, 100, 255);
+
     }
 }
