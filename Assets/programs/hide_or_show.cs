89982e64099b754b96395513c1ec27a81bbdb49c
yuuki1293 <yuuki1293@outlook.jp>
2020-11-11 18:44:28 +0900

    グラデーション

--- Assets/programs/hide_or_show.cs
+++ Assets/programs/hide_or_show.cs
@@ -7,12 +7,17 @@ public class hide_or_show : MonoBehaviour
 {
     public int num;
     SpriteRenderer mr;
+    float dt = 0;
+    float gt = 0.95f;
+    float nega = -1;
 
     // Update is called once per frame
     void Update()
     {
+        dt += nega * Time.deltaTime;
+        if (dt > gt || dt < 0.05) nega *= -1;
         mr = GetComponent<SpriteRenderer>();
-        if(Bv.push_flag[num])mr.material.color = new Color32(0,0,0,255);
-        else mr.material.color = new Color32(0,0,0,0);
+        if (Bv.push_flag[num]) mr.material.color = new Color32(0, 255, 10, (byte)(dt * 255));
+        else mr.material.color = new Color32(255, 100, 100, 0);
     }
 }
