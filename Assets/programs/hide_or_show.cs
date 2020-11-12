fe6a0ca4dbdf65d9dd6dac2ba13d479a128a42a3
yuuki1293 <yuuki1293@outlook.jp>
2020-11-12 17:28:42 +0900

    最大化時に色が変わらない

--- Assets/programs/hide_or_show.cs
+++ Assets/programs/hide_or_show.cs
@@ -17,6 +17,7 @@ public class hide_or_show : MonoBehaviour
         dt += nega * Time.deltaTime;
         if (dt > gt || dt < 0.05) nega *= -1;
         mr = GetComponent<SpriteRenderer>();
+        Debug.Log(dt);
         if (Bv.push_flag[num]) mr.material.color = new Color32(0, 255, 10, (byte)(dt * 255));
         else mr.material.color = new Color32(255, 100, 100, 0);
     }
