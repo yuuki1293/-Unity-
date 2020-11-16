8342d597c7abca6ced172b96d2f79e23117a64df
yuuki1293 <yuuki1293@outlook.jp>
2020-11-16 18:13:19 +0900

    del using

--- Assets/programs/roulette_child.cs
+++ Assets/programs/roulette_child.cs
@@ -5,9 +5,11 @@ using Bluetooth_value;
 
 public class roulette_child : MonoBehaviour
 {
-    const int 一の位 = 0;
-    const int 十の位 = 1;
-    const int 百の位 = 2;
+    /*
+    一の位 = 0;
+    十の位 = 1;
+    百の位 = 2;
+    */
     public int num;
     public Text te;
 
